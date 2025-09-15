156ee6dee27ded1304dbb3dbdc007d80564cdf06
Andrii Sabinin <sabinin.andrii@gmail.com>
2025-09-15 17:52:27 +0200

    Initial Commit

---
+++ NumbersFactsApp/NumbersFactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersFactsAppApp.swift
+//  NumbersFactsApp
+//
+//  Created by Andrii Sabinin on 15.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct NumbersFactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
